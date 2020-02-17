6014672749cb17b68275a926905aee2428891e00
aposipa <andrew.posipanka@gmail.com>
2020-02-17 16:34:13 -0500

    further changes to add a reset button to scoreboard.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 const [lionscore, setLionScore] = useState(0);
 const [tigerscore, setTigerScore] = useState(0);
+const [quarter, setQuarter] = useState(1);
 
 
   return (
@@ -33,6 +34,10 @@ const [tigerscore, setTigerScore] = useState(0);
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button onClick = {() => setLionScore(lionscore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
           <button onClick = {() => setLionScore(lionscore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+        </div>
+        <div>
+          <button onClick ={() => (setLionScore(0), setTigerScore(0))} className='buttons'>Reset Score</button>
+          <button onClick ={() => setQuarter(quarter + 1)} className ='buttons'>Quarter</button>
         </div>
         <div className="awayButtons">
           <button onClick = {() => setTigerScore(tigerscore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
